7f36aeab6dfb40c16ee81170e4544ced6caac261
Turja Kundu <turja16@gmail.com>
2025-08-04 17:18:40 -0500

    Update train_criticaldata_othergnns.py

--- Heterophilic_Benchmarks/GloGNN_repo/train_criticaldata_othergnns.py
+++ Heterophilic_Benchmarks/GloGNN_repo/train_criticaldata_othergnns.py
@@ -33,7 +33,7 @@ def normalize_tensor_sparse(mx, symmetric=0):
 
 """
 def sparse_mx_to_torch_sparse_tensor(sparse_mx):
-    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    #Convert a scipy sparse matrix to a torch sparse tensor.
     sparse_mx = sparse_mx.tocoo().astype(np.float32)
     indices = torch.from_numpy(
         np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
