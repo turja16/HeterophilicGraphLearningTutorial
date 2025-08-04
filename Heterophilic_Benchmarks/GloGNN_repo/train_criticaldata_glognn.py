a922053e2f5127128361b07c29d11ea9a5275c3b
Turja Kundu <turja16@gmail.com>
2025-08-04 18:00:22 -0500

    Update train_criticaldata_glognn.py

--- Heterophilic_Benchmarks/GloGNN_repo/train_criticaldata_glognn.py
+++ Heterophilic_Benchmarks/GloGNN_repo/train_criticaldata_glognn.py
@@ -30,16 +30,28 @@ def normalize_tensor_sparse(mx, symmetric=0):
         mx = mx.dot(r_mat_inv).transpose().dot(r_mat_inv)
         return mx
 
-
+"""
 def sparse_mx_to_torch_sparse_tensor(sparse_mx):
-    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    #"""Convert a scipy sparse matrix to a torch sparse tensor."""
     sparse_mx = sparse_mx.tocoo().astype(np.float32)
     indices = torch.from_numpy(
         np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
     values = torch.from_numpy(sparse_mx.data)
     shape = torch.Size(sparse_mx.shape)
     return torch.sparse.DoubleTensor(indices, values, shape)
+"""
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
 
+    indices = torch.tensor(
+        np.vstack((sparse_mx.row, sparse_mx.col)), dtype=torch.long)
+
+    values = torch.tensor(sparse_mx.data, dtype=torch.float32)
+
+    shape = torch.Size(sparse_mx.shape)
+
+    return torch.sparse_coo_tensor(indices, values, size=shape, dtype=torch.float32)
 
 @torch.no_grad()
 def accuracy(pr_logits, gt_labels):
